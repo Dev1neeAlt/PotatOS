50a1f648e8e0bf2ce3d9ea2970cee72309bd949b
Dev1neeAlt <34199473+Dev1neeAlt@users.noreply.github.com>
2017-12-02 19:30:56 -0500

    Update and rename src to main.js

---
+++ main.js
@@ -0,0 +1,28 @@
+const electron = require('electron');
+const { app, BrowserWindow, Menu } = electron;
+
+
+app.on('ready', function() {
+  notifier.notify('hi');
+  const startWindow = new BrowserWindow({
+    width: 300,
+    height: 300,
+    frame: false
+  });
+  startWindow.loadURL(`file://${__dirname}/start.html`);
+
+
+  const shortcutsMenu = Menu.buildFromTemplate(shortcuts);
+  Menu.setApplicationMenu(shortcutsMenu);
+
+});
+
+const shortcuts = [
+  { 
+    label: 'Quit',
+    accelerator: 'Alt+F4',
+    click() {
+      app.quit();
+    }
+  }
+];
